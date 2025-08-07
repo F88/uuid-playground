2d0425b7af21487359adb74ec5b7e20cd39d9dbb
F88 <685250+F88@users.noreply.github.com>
2025-08-07 16:23:18 +0900

    feat: enhance ESLint configuration with additional plugins and stricter rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,42 @@
+import eslintConfigPrettier from 'eslint-config-prettier';
+import reactDom from 'eslint-plugin-react-dom';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
+import reactX from 'eslint-plugin-react-x';
+import storybook from "eslint-plugin-storybook";
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   {
-    ignores: ['dist', 'coverage'],
+    ignores: [
+      //
+      'dist',
+      'coverage',
+      // '.storybook',
+      'vitest.shims.d.ts',
+      'vitest.workspace.ts',
+    ],
   },
   {
     files: ['**/*.ts', '**/*.tsx'],
     extends: [
-      ...tseslint.configs.recommended,
+      // Remove ...tseslint.configs.recommended and replace with this
+      ...tseslint.configs.recommendedTypeChecked,
+      // Alternatively, use this for stricter rules
+      ...tseslint.configs.strictTypeChecked,
+      // Optionally, add this for stylistic rules
+      ...tseslint.configs.stylisticTypeChecked,
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      // other options...
+      parser: tseslint.parser,
+      parserOptions: {
+        project: ['./tsconfig.node.json', './tsconfig.app.json'],
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
     plugins: {
       'react-hooks': reactHooks,
@@ -29,4 +51,37 @@ export default tseslint.config(
       '@typescript-eslint/no-non-null-assertion': 'off',
     },
   },
+
+
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    plugins: {
+      // Add the react-x and react-dom plugins
+      'react-x': reactX,
+      'react-dom': reactDom,
+    },
+    rules: {
+      // other rules...
+      // Enable its recommended typescript rules
+      ...reactX.configs['recommended-typescript'].rules,
+      ...reactDom.configs.recommended.rules,
+      // Put rules you want to override here
+      'react-dom/no-dangerously-set-innerhtml': 'warn',
+      // Put rules you want to override here
+      'react-x/no-class-component': 'warn',
+    },
+  },
+
+  /**
+   * storybookjs/eslint-plugin-storybook: 🎗Official ESLint plugin for Storybook
+   * https://github.com/storybookjs/eslint-plugin-storybook
+   */
+  storybook.configs["flat/recommended"] /* Storybook's recommended rules */,
+
+  // prettier/eslint-config-prettier: Turns off all rules that are unnecessary or might conflict with Prettier.
+  // https://github.com/prettier/eslint-config-prettier?tab=readme-ov-file#cli-helper-tool
+  //
+  // With the new ESLint “flat config” format, you can control what things override what yourself.
+  // One way of solving the above conflict is to reorder the config objects so that eslint-config-prettier is last:
+  eslintConfigPrettier, // eslint-config-prettier last
 );
